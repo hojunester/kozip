8d2811f4bcabcb9135bf5ac46dc85b0fb070c100
hojunester <hojunester@gmail.com>
2013-12-05 09:42:20 +0900

    changed the top line to specify python3. Oops

--- kozip.py
+++ kozip.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # this script supports shift-jis and euc-kr encodings (basically, Microsoft's Korean and Japanese encoding.)
 
